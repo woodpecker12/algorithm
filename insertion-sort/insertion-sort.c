d5759ff3b46415c0e4c6fccd639d24fadea668e6
zhuohuiWang <woodpecker12@163.com>
2014-09-04 17:38:39 +0800

    添加插入排序的实现，test-1

---
+++ insertion-sort/insertion-sort.c
@@ -0,0 +1,40 @@
+/* 插入排序
+ *
+ * 伪代码(从小到大):
+ * INSERTION-SORT(A)
+ * for j = 2 to A.length
+ * 		key = A[j]
+ *		// Insert A[j] into the sorted sequence A[1...j - 1]
+ * 		i = j - 1 // i 等于 j的前一个
+ * 		while i > 0 and A[i] > key // 当i > 0并且A[i] > A[j]时
+ *				A[i+1] = A[i] // 将大的和小的交换
+ *				i = i - 1
+ *		A[i + 1] = key // 将A[j]插入到合适的地方
+ */
+ 
+ #include <stdio.h>
+ 
+ void insertion_sort(int *sequence)
+ {
+	int i, j, key;
+	for (j = 1; j < sizeof(sequence); j++){
+		key = sequence[j];
+		i = j -1;
+		
+		while ((i >= 0) && (sequence[i] > key)){
+			sequence[i + 1] = sequence[i];
+			i--;
+		}
+	sequence[i + 1] = key;
+	}
+ }
+ 
+ int main(char argv, char **argc)
+ {
+	int a[] = {5, 2, 4, 6, 1, 3};
+	
+	insertion_sort(a);
+	
+	for (i = 0; i < sizeof(a); i++)
+		printf("a[%d] = %d\n", i, a[i]);
+ }
